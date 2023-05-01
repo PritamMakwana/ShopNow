756eb14997cccc51ae3e9a8d217e400b6d6ccc36
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2023-05-01 11:40:16 +0530

    Brands -Edit and delete using livewire

--- resources/views/livewire/admin/brand/modal-form.blade.php
+++ resources/views/livewire/admin/brand/modal-form.blade.php
@@ -4,7 +4,8 @@
         <div class="modal-content">
             <div class="modal-header">
                 <h1 class="modal-title fs-5" id="staticBackdropLabel">Add Brands</h1>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"
+                    wire:click="closeModal"></button>
             </div>
             <form wire:submit.prevent="storeBrand">
                 <div class="modal-body">
@@ -12,29 +13,118 @@
                         <label>Brand Name</label>
                         <input type="text" wire:model.defer="name" class="form-control">
                         @error('name')
-                        <small class="text-danger">{{$message}}</small>
+                            <small class="text-danger">{{ $message }}</small>
                         @enderror
                     </div>
                     <div class="mb-3">
                         <label>Brand Slug</label>
                         <input type="text" wire:model.defer="slug" class="form-control">
                         @error('slug')
-                        <small class="text-danger">{{$message}}</small>
+                            <small class="text-danger">{{ $message }}</small>
                         @enderror
                     </div>
                     <div class="mb-3">
                         <label>Status</label>
                         <input type="checkbox" wire:model.defer="status" />Checked=Hidden,Un-Checked=Visible
                         @error('status')
-                        <small class="text-danger">{{$message}}</small>
+                            <small class="text-danger">{{ $message }}</small>
                         @enderror
                     </div>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"
+                        wire:click="closeModal">Close</button>
                     <button type="submit" class="btn btn-primary">Save</button>
                 </div>
             </form>
+        </div>
+    </div>
+</div>
+
+
+{{-- update Brand model --}}
+
+<div wire:ignore.self class="modal fade" id="updateBrandModal" data-bs-backdrop="static" data-bs-keyboard="false"
+    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Edit Brands</h1>
+                <button type="button" wire:click="closeModal" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div wire:loading class="p-2">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>Loading...
+            </div>
+            <div wire:loading.remove>
+                <form wire:submit.prevent="updateBrand">
+                    <div class="modal-body">
+                        <div class="mb-3">
+                            <label>Brand Name</label>
+                            <input type="text" wire:model.defer="name" class="form-control">
+                            @error('name')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label>Brand Slug</label>
+                            <input type="text" wire:model.defer="slug" class="form-control">
+                            @error('slug')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label>Status</label>
+                            <input type="checkbox" wire:model.defer="status" />Checked=Hidden,Un-Checked=Visible
+                            @error('status')
+                                <small class="text-danger">{{ $message }}</small>
+                            @enderror
+                        </div>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" wire:click="closeModal" class="btn btn-secondary"
+                            data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Edit</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+{{-- Delete Brand model --}}
+
+<div wire:ignore.self class="modal fade" id="deleteBrandModal" data-bs-backdrop="static" data-bs-keyboard="false"
+    tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Delete Brand</h1>
+                <button type="button" wire:click="closeModal" class="btn-close" data-bs-dismiss="modal"
+                    aria-label="Close"></button>
+            </div>
+            <div wire:loading class="p-2">
+                <div class="spinner-border text-primary" role="status">
+                    <span class="visually-hidden">Loading...</span>
+                </div>Loading...
+            </div>
+            <div wire:loading.remove>
+                <form wire:submit.prevent="destroyBrand">
+                    <div class="modal-body">
+                       <h4>Are you sure you want  to delete this data ?</h4>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" wire:click="closeModal" class="btn btn-secondary"
+                            data-bs-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Yes. Delete</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
 </div>
