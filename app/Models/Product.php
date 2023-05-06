9ba3a2b0279011992ac13d2cfe39baca9491010c
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2023-05-06 21:34:17 +0530

    Product read ,update and delete with images

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -30,6 +30,9 @@ class Product extends Model
           'meta_description',
     ];
 
+    public function  category(){
+        return $this->belongsTo(Category::class,'category_id','id');
+    }
     public function  productImages(){
         return $this->hasMany(ProductImage::class,'product_id','id');
     }
