7ffe4af0b8a4dfbccddcba7b07182cb1ed854aa9
Pritam Makwana <85328035+PritamMakwana@users.noreply.github.com>
2023-05-13 19:14:15 +0530

    display products by category with Title & Meta tags w/ relationship

--- resources/views/frontend/collections/category/index.blade.php
+++ resources/views/frontend/collections/category/index.blade.php
@@ -13,7 +13,7 @@
             @forelse ($categories as $categoryItem)
             <div class="col-6 col-md-3">
                 <div class="category-card">
-                    <a href="">
+                    <a href="{{url('/collections/'.$categoryItem->slug)}}">
                         <div class="category-card-img">
                             <img src="{{asset("$categoryItem->image")}}" class="w-100" alt="image">
                         </div>
